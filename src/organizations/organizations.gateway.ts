1d17e625261d37696985d14be937a9c73e204e12
WasathatLayoutIndex <wasath.theekshana@layoutindex.lk>
2023-08-11 15:00:58 +0530

    feat: find all organizations feature add

--- src/organizations/organizations.gateway.ts
+++ src/organizations/organizations.gateway.ts
@@ -3,22 +3,29 @@ import {
   SubscribeMessage,
   MessageBody,
   WebSocketServer,
+  OnGatewayConnection,
 } from '@nestjs/websockets';
 import { OrganizationsService } from './organizations.service';
 import { CreateOrganizationDto } from './dto/create-organization.dto';
 import { Server } from 'socket.io';
 import { Organization } from './schema/organization.schema';
-
+import { Socket } from 'socket.io';
 @WebSocketGateway(3001, {
   cors: {
     origin: '*',
   },
 })
-export class OrganizationsGateway {
+export class OrganizationsGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
 
   constructor(private readonly organizationsService: OrganizationsService) {}
+
+  async handleConnection(client: Socket){
+    const allOrganizations = await this.organizationsService.findAll();
+    client.emit('allOrganizations', allOrganizations);
+  }
+
 
   @SubscribeMessage('createOrganization')
   async create(
